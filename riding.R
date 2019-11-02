9d214ecd7d7eeb14452ca218b85e53832d17c5a5
Tuija Sonkkila <tts@users.noreply.github.com>
2019-11-02 17:17:27 +0200

    Added downloading and unzipping and renamed datasets

--- riding.R
+++ riding.R
@@ -1,14 +1,30 @@
 library(sf)
 library(tidyverse)
 
+roads <-"https://kartta.hel.fi/avoindata/Helsinki_liikennevaylat_avoin_data.zip"
+
+temp <- tempfile()
+download.file(roads, temp)
+unzip(zipfile = temp)
+unlink(temp)
+
+areas <- "http://www.hel2.fi/tietokeskus/data/kartta_aineistot/PKS_Kartta_Rajat_KML2011.zip"
+temp <- tempfile()
+download.file(areas, temp)
+unzip(zipfile = temp)
+unlink(temp)
+
 class(st_geometry(st_read("Helsinki_liikennevaylat_avoin_data/KML/Hki_liikennevaylat.kml", quiet = TRUE)))
 # "sfc_LINESTRING" "sfc"
 
-data <- st_read("Helsinki_liikennevaylat_avoin_data/KML/Hki_liikennevaylat.kml")
+class(st_geometry(st_read("PKS_suuralue.kml", quiet = TRUE)))
+# "sfc_POLYGON" "sfc" 
 
-districts <- st_read("PKS_suuralue.kml")
+roads_data <- st_read("Helsinki_liikennevaylat_avoin_data/KML/Hki_liikennevaylat.kml")
 
-hki <- districts %>%
+areas_data <- st_read("PKS_suuralue.kml")
+
+hki <- areas_data %>%
   filter(Name %in% c("Läntinen", "Keskinen","Pohjoinen")) %>%
   mutate(Name_en = case_when(
     Name == "Läntinen" ~ "West",
@@ -16,14 +32,14 @@ hki <- districts %>%
     Name == 'Pohjoinen' ~ "North",
   ))
 
-riding <- data %>% 
+riding_tracks <- roads_data %>% 
   filter(str_detect(Description, "Ratsastus.*"))
 
-dist <- riding %>% 
+lengths <- riding_tracks %>% 
   mutate(l = st_length(st_cast(st_sfc(geometry),"LINESTRING"))) %>% 
   summarise(total_length = sum(l))
 
-track_length <- as.numeric(dist$total_length)
+track_length <- as.numeric(lengths$total_length)
 # 8442.004 m
 
 # Had troubles in pasting the track_length in the title
@@ -33,14 +49,16 @@ p <- ggplot(hki) + geom_sf(alpha = 0.3, color = "slategray2") +
   geom_sf_text(aes(label = Name_en), colour = "slategray3") +
   geom_point(aes(y=60.1704507, x=24.9386955), colour="red", size = 1) 
 
-p <- p + geom_sf(data = riding, color = "brown", linetype = "dotdash") + 
+p <- p + geom_sf(data = riding_tracks, color = "brown", linetype = "dotdash") + 
+  ylab("Latitude") + 
+  xlab("Longitude") +
   theme(panel.grid.major = element_line(color = gray(.5), linetype = "dashed",
                                         size = 0.5), 
         panel.background = element_rect(fill = "aliceblue")) +
   labs(x = NULL, y = NULL, 
        title = chart_title,
        subtitle = "The lonely dot is Helsinki Central Station",
-       caption = "Data sources: Helsingin liikenneväylät, Helsingin piirijako\nhttps://hri.fi/data/dataset/helsingin-liikennevaylat https://hri.fi/data/dataset/helsingin-piirijako")
+       caption = "Data sources: Helsingin liikenneväylät, Helsingin aluejakokartat\nhttps://hri.fi/data/dataset/helsingin-liikennevaylat https://hri.fi/data/fi/dataset/paakaupunkiseudun-aluejakokartat")
 p
 
 ggsave(
